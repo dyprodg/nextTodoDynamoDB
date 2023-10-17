7dffa20b8babbc25e0052a12e1b7f79b7f3a6682
dyprodg <dennisdiepolder@outlook.com>
2023-10-17 22:27:31 +0200

    new todo angepasst

--- app/page.js
+++ app/page.js
@@ -35,7 +35,7 @@ export default function Home() {
       };
 
       try {
-        const response = await fetch('/api/newtodo', {
+        await fetch('/api/newtodo', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
